8f8b104759e3af1dd1fd7506cd5f5d5c943ae484
Ben Senescu <senescu.b@husky.neu.edu>
2020-01-07 19:43:09 -0500

    Upload Image Working. Retreiveing images currently is a 403

--- src/components/profile/ProfileUploadMedia.vue
+++ src/components/profile/ProfileUploadMedia.vue
@@ -1,41 +1,147 @@
 <template>
     <div>
         <h2>Upload Media</h2>
-        <amplify-photo-picker
-            :photoPickerConfig="photoPickerConfig"
-            v-on:fileUpload="fileUpload"/>
-        <div class="image-container">
-            <img :src="photoUrl" />
-        </div>
+        <!-- <amplify-photo-picker
+          :photoPickerConfig="photoPickerConfig"
+          v-on:fileUpload="localMedia.s3Path = $event"/> -->
+        <img v-if="isFileImage" :src="photoUrl" class="image-container"/>
+        <input type="file" ref="fileInput" accept="image/*, video/*" v-on:change="pick">
+        <input type="text" placeholder="Name" v-model="localMediaMeta.name">
+        <input type="text" placeholder="Description" v-model="localMediaMeta.description">
+        <button :disabled="saveFileLoading" v-on:click="saveFile">Submit</button>
+        <!-- <amplify-s3-image
+          v-for="mediaFile in mediaFiles"
+          :key="mediaFile.id"
+          :imagePath="`/${mediaFile.s3Path}`" /> -->
     </div>
 </template>
 
 <script>
 import { Storage } from 'aws-amplify';
+import { mapState } from 'vuex';
+import Api from '../../services/api';
 
 export default {
   name: 'profile-upload-media',
   data() {
     return {
-      photoPickerConfig: {
-        path: 'media/',
-      },
       photoUrl: '',
+      file: null,
+      localMediaMeta: {
+        s3Path: '',
+        name: '',
+        description: '',
+      },
+      mediaFiles: [],
+      mediaUrls: [],
+      saveFileLoading: false,
+      error: '',
     };
   },
   created() {
-    Storage.get('media/taneli-lahtinen-EGbnaltr0v4-unsplash.jpg')
-      .then((url) => {
-        this.photoUrl = url;
-      })
-      .catch((error) => {
-        console.error(error);
+    Api.getMedia()
+      .then((res) => {
+        this.mediaFiles = res;
       });
+    // Promise.all(this.mediaFiles.map(async ({ s3Path }) => {
+    //   const url = await Storage.get(s3Path)
+    //     .catch((error) => {
+    //       this.error = error;
+    //     });
+    //   console.log('url', url);
+    //   return url;
+    // }))
+    //   .then((res) => {
+    //     this.mediaUrls = res;
+    //   })
+    //   .catch((error) => {
+    //     this.error = error;
+    //   });
+  },
+  computed: {
+    ...mapState('user', {
+      user: 'user',
+    }),
+    isFileImage() {
+      const mediaType = this.getMediaType(this.localMediaMeta.s3Path);
+      return mediaType === '.jpg' || mediaType === '.png';
+    },
   },
   methods: {
-    fileUpload() {
-    //   console.log('fileUploaded');
-    //   console.log(path);
+    initData() {
+      this.file = null;
+      this.$refs.fileInput.value = null;
+      this.localMediaMeta = {
+        s3Path: '',
+        name: '',
+        description: '',
+      };
+      this.photoUrl = '';
+    },
+    // this method was adapted from the photopicker component in aws-amplify-vue
+    pick(evt) {
+      [this.file] = evt.target.files;
+      if (!this.file) { return; }
+      const fileName = this.file.name;
+      this.localMediaMeta.s3Path = `media/${fileName}`;
+      const that = this;
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const { target } = e;
+        const url = target.result;
+        // set image url to show what the user has upload if its an image
+        that.photoUrl = url;
+      };
+      reader.readAsDataURL(this.file);
+    },
+    async uploadToS3() {
+      try {
+        await Storage.put(
+          this.localMediaMeta.s3Path,
+          this.file,
+          {
+            header: this.$Amplify.I18n.get('File Upload'),
+            title: this.$Amplify.I18n.get('Upload'),
+            accept: '*/*',
+            storageOptions: {},
+          },
+        );
+      } catch (error) {
+        this.error = error;
+      }
+    },
+    async saveFileMetaData() {
+      try {
+        const mediaType = this.getMediaType(this.localMediaMeta.s3Path);
+        const mediaBody = {
+          username: this.user.username,
+          s3Path: this.localMediaMeta.s3Path,
+          name: this.localMediaMeta.name,
+          description: this.localMediaMeta.description,
+          mediaType,
+        };
+
+        const media = await Api.createMedia(mediaBody);
+        console.log('fileUploaded', media);
+        this.mediaFiles.unshift(media);
+      } catch (error) {
+        this.error = error;
+      }
+    },
+    async saveFile() {
+      try {
+        this.saveFileLoading = true;
+        await this.uploadToS3();
+        await this.saveFileMetaData();
+        this.saveFileLoading = false;
+        this.initData();
+      } catch (error) {
+        this.error = error;
+      }
+    },
+    getMediaType(filePath) {
+      // TODO do this with regex for all file types
+      return filePath.slice(filePath.length - 4, filePath.length);
     },
   },
 };
---
+++ src/services/api.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3000/api';
+
+async function createMedia(media) {
+  try {
+    const { data } = await axios.post(`${baseUrl}/media`, media);
+    return data;
+  } catch (error) {
+    return { error: `Create Media Failed: ${error}` };
+  }
+}
+
+async function getOneMedia(username, mediaId) {
+  try {
+    const { data } = await axios.get(`${baseUrl}/media/${mediaId}`, { username });
+    return data;
+  } catch (error) {
+    return { error: `Get One Media Failed: ${error}` };
+  }
+}
+
+async function getMedia() {
+  try {
+    const { data } = await axios.get(`${baseUrl}/media`);
+    return data;
+  } catch (error) {
+    return { error: `Get Media Failed: ${error}` };
+  }
+}
+
+// async function updateMedia(payload) {
+
+// };
+
+async function deleteMedia(username, mediaId) {
+  try {
+    const { data } = await axios.delete(`${baseUrl}/media/${mediaId}`, { username });
+    return data;
+  } catch (error) {
+    return { error: `Delete Media Failed: ${error}` };
+  }
+}
+
+export default {
+  createMedia,
+  getOneMedia,
+  getMedia,
+  // updateMedia,
+  deleteMedia,
+};
