306c3cb42b1e9b2d81dc824beb676216d9305541
Ben Senescu <senescu.b@husky.neu.edu>
2020-01-23 19:45:23 -0500

    Updated home page for waiting for ssl cert

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <template functional >
-  <div class="home">
-    <div class="imageContainer">
+  <!-- <div class="home"> -->
+  <div class="flex-col">
+    <h3>Waiting on SSL Certifacte from ACM for my api...</h3>
+    <h3>The project should be up and running by 1/27.</h3>
+    <h3>In the meantime, check out my github repos linked below:</h3>
+    <a href="https://github.com/bensenescu/aws-media-project">Vue.js Frontend</a>
+    <a href="https://github.com/bensenescu/media-project-api">Node.js Api</a>
+    <!-- <div class="imageContainer">
       <h2>Photos</h2>
       <router-link :to="{name: 'photos-view'}">
         <img src="https://media-projecte3577ba9ab8c460ebc91cb311da648f6dev-dev.s3.amazonaws.com/public/static/desert.jpg" />
@@ -11,7 +17,7 @@
       <img
         src="https://media-projecte3577ba9ab8c460ebc91cb311da648f6dev-dev.s3.amazonaws.com/public/static/ferris-wheel.jpg"
         style="cursor: not-allowed"/>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -41,5 +47,11 @@ export default {
     top: 20%;
     color: @mediaBrown;
   }
+}
+.flex-col {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-top: 15%;
 }
 </style>
