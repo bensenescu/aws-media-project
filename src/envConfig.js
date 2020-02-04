3aa0970295b3538be709d5cdd0ec7ee0968df89e
Ben Senescu <senescu.b@husky.neu.edu>
2020-02-04 17:33:34 -0500

    updated for ssl cert

--- src/envConfig.js
+++ src/envConfig.js
@@ -4,7 +4,7 @@ let apiUrl = '';
 if (deploy === 'dev') {
   apiUrl = 'http://localhost:8081/api';
 } else if (deploy === 'prod') {
-  apiUrl = 'http://media-project.us-east-1.elasticbeanstalk.com/api';
+  apiUrl = 'https://api.awsmediaproject.com/api';
 }
 
 export default {
